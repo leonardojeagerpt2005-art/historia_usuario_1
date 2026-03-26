e47a701e1d07e9a7e0adc75d972b744e40bfe4a4
leonardo perez <leonardojeagerpt2005@gmail.com>
2026-03-26 16:48:03 -0500

    second commit

--- inventario.py
+++ inventario.py
@@ -1,26 +1,32 @@
 # --- Purchase Calculation Script ---
-
+continuar_ciclo = True
 # Request product details from the user
-product_name = input("Enter the product to buy: ")
-price = int(input("Enter the product price: "))
-quantity = float(input("Enter the quantity to buy: "))
+while continuar_ciclo :
+    product_name = input("Enter the product to buy: ")
+    price = float(input("Enter the product price: "))
+    quantity = int(input("Enter the quantity to buy: "))
 
-# Validation: Ensure both price and quantity are positive numbers
-if price < 0:
-    print("Invalid numerical value. Please enter a positive number for price.")
-elif quantity < 0:
-    print("Invalid numerical value. Please enter a positive number for quantity.")
-else:
-    # Calculate the total cost if inputs are valid
-    total_cost = price * quantity
-    
-    # Display the summary to the user
-    print(f"Your total is: {total_cost}")
-    print(f"""
-    --- Invoice Summary ---
-    Product: {product_name} 
-    Unit Price: {price}
-    Quantity: {quantity}
-    Total Cost: {total_cost}
-    -----------------------
-    """)
+    # Validation: Ensure both price and quantity are positive numbers
+    if price <= 0:
+        print("Invalid numerical value. Please enter a positive number for price.")
+        continue
+    elif quantity <= 0:
+        print("Invalid numerical value. Please enter a positive number for quantity.")
+        continue
+    else:
+        # Calculate the total cost if inputs are valid
+        total_cost = price * quantity
+        continuar_ciclo = input("Do you want to calculate another product? (yes/no): ").strip().lower()
+        if continuar_ciclo == 'no':
+            continuar_ciclo = False
+        
+        # Display the summary to the user
+        print(f"Your total is: {total_cost}")
+        print(f"""
+        --- Invoice Summary ---
+        Product: {product_name} 
+        Unit Price: {price}
+        Quantity: {quantity}
+        Total Cost: {total_cost}
+        -----------------------
+        """)
